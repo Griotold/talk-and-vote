cb5b6065d738107e986dacda0ac41fa6a37a4f0b
griotold <gotjd9773@naver.com>
2025-10-08 11:39:51 +0900

    7-1. FastAPI 초기 설정 - Alembic 주석 처리

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,36 @@
 # pyright: reportMissingImports=false
+import os
+import uvicorn
+from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.db.database import Base, async_engine
+from fastapi.concurrency import asynccontextmanager
 
-app = FastAPI()
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), "..", ".env"))
 
-# @ 는 데코레이터
-@app.get("/")
-async def root():
-    return {"message": "Hello, fastAPI!"}
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("🚀 Application starting...")
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    print("👋 Application shutting down...")
+    await async_engine.dispose()
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
